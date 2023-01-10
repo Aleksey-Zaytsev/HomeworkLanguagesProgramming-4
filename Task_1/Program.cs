1dbe903890c48b23348c858c387a858563084737
Aleksey <a.y.zayitsev@gmail.com>
2023-01-10 23:36:27 +0300

    Степень числа  с натуральным показателем

---
+++ Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write ("Введите первое число: ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+int result = 1;
+  for(int i=1; i <= num2; i++)
+  {
+    result = result * num1;
+  }
+  Console.WriteLine ($"Степень числа {num1} с натуральным показателем {num2} равно: {result}");
