2c03dc9ba24d104189a3644201eb9bada41aeda8
Aleksey <a.y.zayitsev@gmail.com>
2023-01-11 00:18:47 +0300

    Программа, которая выдает сумму цифр в числе

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int num1 = num;
+
+while (num > 0)
+{
+    int num2 = num % 10;
+    num = num / 10;
+    sum = sum + num2;
+}
+Console.WriteLine($"Сумма цифр числа {num1} равна: {sum}");
